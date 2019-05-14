d8635ce1c015901838d51d00165df69b4524f11d
rodesousa <dessroberto@gmail.com>
2019-05-14 13:23:35 +0200

    add some packets
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/bash
 
 apt-get install -y \
- 	python3-dev python3-pip python3-setuptools \
- 	python-dev python-pip python-setuptools \
-	tree curl fzf tig
+	python3-dev python3-pip python3-setuptools \
+	python-dev python-pip python-setuptools \
+	tree curl fzf tig \
+	apt-transport-https \
+	ca-certificates \
+	gnupg-agent \
+	software-properties-common
 
 snap install docker
 snap install kubectl --classic
@@ -13,3 +17,8 @@ snap install go --classic
 snap install node --edge --classic
 
 npm install diff-so-fancy -g
+
+# docker
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
