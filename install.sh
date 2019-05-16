5ed6ff555346e6ce180e6ad488d6584688e4fdf3
rodesousa <dessroberto@gmail.com>
2019-05-16 16:00:37 +0200

    add elixir
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/bash
 
+# elixir
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+
+apt update
+
 apt-get install -y \
 	python3-dev python3-pip python3-setuptools \
 	python-dev python-pip python-setuptools \
 	tree curl fzf tig \
-	apt-transport-https \
-	ca-certificates \
-	gnupg-agent \
-	software-properties-common
+	apt-transport-https ca-certificates gnupg-agent software-properties-common \
+	elixir esl-erlang
 
 snap install docker
 snap install kubectl --classic
