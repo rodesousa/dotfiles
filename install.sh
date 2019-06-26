89b012a3847a74171a1136fc95d9c54c36f9896c
rodesousa <dessroberto@gmail.com>
2019-06-26 17:08:15 +0200

    add inotify-tools packet
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt-get install -y \
 	python-dev python-pip python-setuptools \
 	tree curl fzf tig \
 	apt-transport-https ca-certificates gnupg-agent software-properties-common \
-	elixir esl-erlang
+	elixir esl-erlang inotify-tools
 
 snap install docker
 snap install kubectl --classic
