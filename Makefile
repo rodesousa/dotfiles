b48fbb6bcbacb0a322dfc1ed7334f5a827567a79
rodesousa <dessroberto@gmail.com>
2019-09-11 16:53:26 +0200

    fix some stuff
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
 .PHONY: packages
 packages:
-	@sudo ./install.sh
-	@pip3 install thefuck
-	@mkdir $(HOME)/bin
+	./install.sh
+	
 
 .PHONY: config
 config:
@@ -13,19 +12,23 @@ config:
 	@ln -snf $(CURDIR)/config/terminator $(HOME)/.config
 	@ln -snf $(CURDIR)/config/git $(HOME)/.config/git
 
+.PHONY: neovim
 neovim:
 	@curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	@pip install pynvim --upgrade
 	@pip3 install pynvim --upgrade
 	@sudo apt install neovim -y
-	@gem install neovim
+	# doesnt work
+	@sudo gem install neovim 
 	@sudo npm install -g neovim
 
+.PHONY: golang
 golang:
 	@mkdir $(HOME)/go
 	@sudo apt-get install go-dep -y
 
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
+.PHONY: k8s
 k8s:
 	@git clone https://github.com/ahmetb/kubectx.git $(HOME)/git/kubectx
 	@ln -snf $(HOME)/git/kubectx/kubectx $(HOME)/bin/kubectlx
--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 # elixir
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 
-apt update
+sudo apt update
 
-apt-get install -y \
+sudo apt-get install -y \
 	python3-dev python3-pip python3-setuptools \
 	python-dev python-pip python-setuptools \
 	tree curl fzf tig silversearcher-ag \
 	apt-transport-https ca-certificates gnupg-agent software-properties-common \
-	elixir esl-erlang inotify-tools ctags
+	elixir esl-erlang inotify-tools ctags terminator
 
+pip3 install thefuck
 
 snap install docker
 snap install kubectl --classic
@@ -20,9 +21,16 @@ snap install ruby --classic
 snap install go --classic
 snap install node --edge --classic
 
-npm install diff-so-fancy -g
+sudo npm install diff-so-fancy -g
 
 # docker
 sudo addgroup --system docker
 sudo adduser $USER docker
 newgrp docker
+
+# config git
+git config --global user.email "dessroberto.gmail.com"
+git config --global user.name "Roberto De Sousa"
+
+# never SUDO !
+mkdir $HOME/bin
